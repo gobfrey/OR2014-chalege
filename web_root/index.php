a2dead2712f8c2725ea2b2d754af9b320d51400c
Adam Field <adam@adamfield.net>
2014-06-10 07:23:17 -0700

    working version

--- web_root/index.php
+++ web_root/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require('../ORCID.php');
+
 $f3=require('../fatfree-master/lib/base.php');
 $f3->config('config.ini');
 
@@ -12,11 +14,18 @@ $f3->run();
 
 function fillmylist($f3)
 {
+	$params = $f3->get('REQUEST');
 
+	$entity_type = $params['entity_type'];
+	$output = $params['output'];
 
+	$function_name = "get_data_$entity_type";
+	$data = call_user_func($function_name, $params);
 
-
+	$function_name = "send_data_$output";
+	call_user_func($function_name, $f3, $data);
 }
+
 
 
 
@@ -27,5 +36,10 @@ function human_inputform($f3)
 	exit;
 }
 
+function send_data_json($f3, $data)
+{
+	header('Content-Type: application/json');
+	echo json_encode($data);
+}
 
 ?>
