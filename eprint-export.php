f74a03d51f033df4476975fddf226f05c7a38dcc
eprints <eprints@eprints-Latitude-E6320>
2014-06-11 11:59:08 +0300

    eprints export auto-fill family, given name and id

--- eprint-export.php
+++ eprint-export.php
@@ -1,5 +1,4 @@
 <?
-
 $data = array(
 'entity_type' => 'person',
 'items' => array(
@@ -65,58 +64,43 @@ array(
 
 
 
-function send_data_eprints($fmldata)
+function send_data_eprints($f3,$fmldata)
 {
-	#print_r($fmldata);
-#	echo count($fmldata["items"]);
-	
-#	echo "\n";
-	
 	$ul = new SimpleXMLElement('<ul/>');
 	foreach ($fmldata["items"] as $item)
 	{
 		$given_name = $item["structured_val"]["given_name"];
 		$family_name = $item["structured_val"]["family_name"];
 		$orcid = $item["id"];
-#		print_r($item["structured_val"]["given_name"]);
-#		print_r($item["structured_val"]["family_name"]);
-#		print_r($item["id"]);
-#		print ($given_name. $family_name. $orcid);
-
 		$li = $ul -> addChild('li');	
-		$li -> addChild("span",$given_name.", ".$family_name." [".$orcid."]");
+			$li -> addChild("span",$given_name.", ".$family_name." [".$orcid."]");
 
 ##adding the autofill data
 
 		$fillul = $li -> addChild("ul");
 		$fillli = $fillul -> addChild("li",$family_name);
-		$fillli -> addAttribute('id', 'for:value:relative:_name_family');
+			$fillli -> addAttribute('id', 'for:value:relative:_name_family');
+			
+		$fillli = $fillul -> addChild("li",$given_name);
+			$fillli -> addAttribute('id', 'for:value:relative:_name_given');
+
+		$fillli = $fillul -> addChild("li",$orcid);
+                        $fillli -> addAttribute('id', 'for:value:relative:_id');
+
 
 	}	
 
-
 	
-#	$rtn = 0;
-#	$xml = new SimpleXMLElement('<xml/>');
-
-#	for ($i = 1; $i <= 8; ++$i) {
- #   		$track = $xml->addChild('track');
-  #  		$track->addChild('path', "song$i.mp3");
-   # 		$track->addChild('title', "Track $i - Track Title");
-#	}
 
 	header('Content-type: text/xml');
 	$rtn = $ul->asXML();
 	echo $rtn;
+}
 
+#	send_data_eprints($data,$data);
 
-#	send_data_eprints($rtn);
+ 
 
-
-} 
-
-
-send_data_eprints($data);
 
 
 ?>
